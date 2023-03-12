41331eb6f586a6e716bfd27058c8b51542681129
intro-dog <metvlad3@gmail.com>
2023-03-12 12:04:10 +0200

    add new homework d

---
+++ homework_8/Size.java
@@ -0,0 +1,29 @@
+package main.java.homework_8;
+
+public enum Size {
+    S("S", 20, 35),
+    M("M", 30, 45),
+    XL("XL", 40, 55);
+
+    private String abbreviation;
+    private int width;
+    private int length;
+
+    String getAbbreviation() {
+        return abbreviation;
+    }
+
+    int getWidth() {
+        return width;
+    }
+
+    int getLength() {
+        return length;
+    }
+
+    Size(String abbreviation, int width, int length) {
+        this.abbreviation = abbreviation;
+        this.width = width;
+        this.length = length;
+    }
+}
